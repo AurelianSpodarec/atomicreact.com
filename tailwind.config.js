74ddea5e6550a1cae690162bb8beb3bbc5716c69
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-10-07 18:39:09 +0100

    update: adding tailwindcss nextjs docs theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
     mode: "jit",
+    darkMode: "class",
     content: [
         "./src/components/**/*.{js,ts,jsx,tsx}", 
         "./src/pages/**/*.{js,ts,jsx,tsx}",
