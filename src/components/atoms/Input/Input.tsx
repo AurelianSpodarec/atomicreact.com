acf0f8b0a688bfda5b8b6b0505c2e3662a889090
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-10-29 15:59:17 +0100

    feat: input basic props

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,10 +1,45 @@
-function Input() {
+function Input(props:InputProps) {
+    
+    const {
+        id,
+        className,
+        placeholder,
+        variant = "primary",
+        kind = "outline",
+        disabled = false,
+        fullWidth = false,
+        label,
+        labelPosition = "top",
+        icon,
+        iconPosition = "right",
+        type = "text",
+        defaultValue,
+        helperText,
+    } = props;
+
     return (
         <>
-            <input />
             <label></label>
+            <input placeholder={placeholder} />
         </>
     );
 }
 
 export default Input;
+
+interface InputProps {
+    id?: string;
+    className?: string;
+    placeholder?: string;
+    variant?: "primary" | "secondary";
+    kind?: "outline" | "filled" | "standard";
+    disabled?: boolean;
+    fullWidth?: boolean;
+    label?: string;
+    labelPosition?: "top" | "right" | "bottom" | "left";
+    icon?: string;
+    iconPosition?: "left" | "right";
+    type?: "number" | "email" | "tel" | "text" | "password" ;
+    defaultValue?: string;
+    helperText?: string;
+}
